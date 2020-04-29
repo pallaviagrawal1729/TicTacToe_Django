51fd3587b2fc38b4c14d995ffdd20f7d7c4164f5
Pallavi Agrawal <wacky.pallavi@gmail.com>
2020-04-29 19:45:58 +0530

    Third Commit includes setup and use of Django's default admin site with features

--- ticTacToe/gamePlay/admin.py
+++ ticTacToe/gamePlay/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Move, Game
+
+
+# visit https://docs.djangoproject.com/en/1.11/ref/contrib/admin/ for doc.
+
+# admin.site.register(Game)
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    # to set which fields you want to see per game
+    list_display = ('id', 'first_player', 'second_player', 'status')
+    # makes status editable and make sure to make it a tuple
+    list_editable = ('status',)
+
+
+admin.site.register(Move)
--- ticTacToe/gamePlay/models.py
+++ ticTacToe/gamePlay/models.py
@@ -6,6 +6,14 @@ Django comes with default User class, Django will add columns based on
 the other side of relation i.e. via ForeignKey reference
 '''
 
+GAME_STATUS_CHOICES = (
+    ('F', 'First Player To Move'),
+    ('S', 'Second Player To Move'),
+    ('W', 'First Player Wins'),
+    ('L', 'Second Player Wins'),
+    ('D', 'Draw')
+)
+
 
 class Game(models.Model):
     first_player = models.ForeignKey(User, related_name="games_first_player", on_delete=models.CASCADE)
@@ -13,6 +21,11 @@ class Game(models.Model):
     # auto_now helps update time-stamp every time
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
+    # choices make a dropDown for the status
+    status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
+
+    def __str__(self):
+        return "{0} Vs {1}".format(self.first_player, self.second_player)
 
 
 class Move(models.Model):
@@ -28,4 +41,3 @@ class Move(models.Model):
     # a many-to-one relation i.e. for a game we will have multiple moves
     # on counter part Django will create a move set to store moves
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-
